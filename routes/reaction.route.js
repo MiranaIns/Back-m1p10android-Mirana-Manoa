284350fe61ba52614c563a16b49e3ddb7087a13a
MiranaIns <89468422+MiranaIns@users.noreply.github.com>
2023-08-07 21:53:33 +0300

    delete route

--- routes/reaction.route.js
+++ routes/reaction.route.js
@@ -4,6 +4,6 @@ const ReactionsController = require('../controllers/reaction.controller');
 const authMiddleware = require("../middlewares/auth.middleware");
 
 router.post('/', authMiddleware(), ReactionsController.createReaction);
-router.post('/', authMiddleware(), ReactionsController.deleteReaction);
+router.post('/delete', authMiddleware(), ReactionsController.deleteReaction);
 
 module.exports = router;
